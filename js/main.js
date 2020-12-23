b4e7b7bfb41a70813687ed030de3c4cd89cfec3b
Celiamf <celiamartinfern@gmail.com>
2020-12-23 17:26:06 +0100

    Add method to render digits on result box

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
 "use strict";
+debugger;
 
 const properties = {
   btns: document.querySelectorAll(".js-btn"),
   action: "",
+  resultBox: document.querySelector(".js-resultBox"),
+  btnValue: "",
 };
-
+debugger;
 const methods = {
   listenToBtns: () => {
     for (let index = 0; index < properties.btns.length; index++) {
-      properties.btns[index].addEventListener("click", methods.pushBtn);
+      properties.btns[index].addEventListener("click", methods.getValue);
     }
   },
-  pushBtn: (event) => {
+  getValue: (event) => {
     properties.action = event.target.getAttribute("class");
-    methods.calculate(properties.action);
+    properties.btnValue += event.target.innerHTML;
+    methods.render(properties.action);
   },
-  calculate: (action) => {
+  render: (action) => {
     if (action.includes("js-number")) {
-      console.log("It's a number");
+      properties.resultBox.innerHTML = properties.btnValue;
     } else if (action.includes("js-symbol")) {
       console.log("It's a symbol");
     } else if (action.includes("js-AC")) {
-      console.log("It's AC");
+      properties.btnValue = "";
     } else if (action.includes("js-equals")) {
       console.log("Equals");
     } else if (action.includes("js-decimal")) {
